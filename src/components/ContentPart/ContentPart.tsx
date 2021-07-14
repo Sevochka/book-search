58e0b516abc1c7219caa83900ce6e9680a22387b
Vsevolod <sevkaplay@gmail.com>
2021-07-14 21:44:43 +0300

    Add ContentPart with list of books

--- src/components/ContentPart/ContentPart.tsx
+++ src/components/ContentPart/ContentPart.tsx
@@ -1,12 +1,27 @@
 import { FC } from 'react';
 import { BookCard } from 'components/BookCard/BookCard';
+import { observer } from 'mobx-react';
+import { useStores } from 'hooks/useStores';
+import './ContentPart.scss';
 
-const ContentPart: FC = () => {
-  return (
-    <>
-      <BookCard />
-    </>
-  );
-};
+const ContentPart: FC = observer(() => {
+  const { bookStore } = useStores();
+  if (!bookStore.books) {
+    return null;
+  }
+  const mapBooks = bookStore.books.map(({ volumeInfo }, i) => {
+    const { authors, title, categories, imageLinks } = volumeInfo;
+    return (
+      <BookCard
+        key={i}
+        authors={authors}
+        title={title}
+        categories={categories}
+        smallThumbnail={imageLinks && imageLinks.smallThumbnail}
+      />
+    );
+  });
+  return <div className="content-part">{mapBooks}</div>;
+});
 
 export { ContentPart };
