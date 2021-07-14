86b85495fbafb1f176b19836e223cb93bb5965d2
Vsevolod <sevkaplay@gmail.com>
2021-07-14 21:33:20 +0300

    Replace first letter of storeContext with a capital one

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BookStore } from 'stores/BookStore';
 
-export const storesContext = React.createContext({
+export const StoresContext = React.createContext({
   bookStore: new BookStore(),
 });
