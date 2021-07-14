b1cfd9e5d04842e636b64c239155624500a5deca
Vsevolod <sevkaplay@gmail.com>
2021-07-14 12:24:21 +0300

    Make test for SearchBar

---
+++ src/components/SearchBar/SearchBar.test.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { SearchBar } from './SearchBar';
+
+describe('SearchBar', () => {
+  const placeholder = 'Test Placeholder',
+    searchText = 'React Book';
+  let executorSpy: jest.Mock;
+
+  beforeEach(() => {
+    executorSpy = jest.fn((value: string) => value);
+    render(
+      <SearchBar
+        inputPlaceholder={placeholder}
+        handleStartSearch={executorSpy}
+      />
+    );
+  });
+  test('expect input with placeholder to be in the document', () => {
+    const inputElement = screen.getByPlaceholderText(placeholder);
+    expect(inputElement).toBeInTheDocument();
+  });
+  test('expect search btn to be in the document', () => {
+    const btnElement = screen.getByText('Search');
+    expect(btnElement).toBeInTheDocument();
+  });
+  test('should clear input on btn-clear click', () => {
+    const btnElement = screen.getByText('Cancel');
+    const inputElement = screen.getByPlaceholderText(placeholder);
+    fireEvent.change(inputElement, {
+      target: {
+        value: searchText,
+      },
+    });
+    fireEvent.click(btnElement);
+    expect((inputElement as HTMLInputElement).value).toBe('');
+  });
+  test('should call executor function with searchText on btn-search click', () => {
+    const btnElement = screen.getByText('Search');
+    const inputElement = screen.getByPlaceholderText(placeholder);
+    fireEvent.change(inputElement, {
+      target: {
+        value: searchText,
+      },
+    });
+    fireEvent.click(btnElement);
+    expect(executorSpy).toBeCalledWith(searchText);
+  });
+});
