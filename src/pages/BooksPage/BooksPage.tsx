50940c06172d1b6032f2d923f753b8b3899ad1d0
Vsevolod <sevkaplay@gmail.com>
2021-07-14 15:37:22 +0300

    Fill BooksPage.tsx with created components

--- src/pages/BooksPage/BooksPage.tsx
+++ src/pages/BooksPage/BooksPage.tsx
@@ -1,22 +1,11 @@
 import './BooksPage.scss';
-import { SearchBar } from 'components/SearchBar/SearchBar';
+import { ConfigurationPart } from 'components/ConfigarationPart/ConfigurationPart';
 
 const BooksPage = () => {
   return (
     <article className="books">
-      <section className="books__configuration">
-        <div className="books__input-group input-group">
-          <SearchBar
-            startSearch={(data) => {
-              console.log(data);
-            }}
-            inputPlaceholder="Search for you favorite books"
-          />
-        </div>
-        <div>
-          <h2>123</h2>
-        </div>
-      </section>
+      <ConfigurationPart className="books__configuration" />
+
       <section className="books__content">
         <h1>1</h1>
       </section>
