3a9fd9d0186d01b7c63275e425ad40f3d5b61b37
Vsevolod <sevkaplay@gmail.com>
2021-07-14 21:33:52 +0300

    Add new layout to BookCard

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,16 +1,43 @@
 import { FC } from 'react';
 import './BookCard.scss';
 
-const BookCard: FC = () => {
+type Props = {
+  title: string;
+  categories?: string[];
+  smallThumbnail?: string;
+  authors?: string[];
+};
+
+const BookCard: FC<Props> = ({
+  smallThumbnail,
+  title,
+  authors,
+  categories,
+}) => {
+  const joinAuthors = authors ? authors.join(', ') : '';
   return (
-    <div className="card book-card">
-      <img src="" className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the cards content.
-        </p>
+    <div className="card mb-3 justify-content-center book-card">
+      <div className="row g-0 align-items-center">
+        <div className="col-md-4">
+          {smallThumbnail && (
+            <img
+              src={smallThumbnail}
+              className="img-fluid rounded-start w-100"
+              alt={title}
+            />
+          )}
+        </div>
+        <div className="col-md-8 h-100">
+          <div className="card-body h-100">
+            <h5 className="card-title fs-6">{title}</h5>
+            <p className="card-text">{joinAuthors}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                {categories ? categories[0] : ''}
+              </small>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
