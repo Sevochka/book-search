a649efb5b258bcc3571a67e450e20ba0ef7ea48a
Vsevolod <sevkaplay@gmail.com>
2021-07-14 15:33:50 +0300

    Add user-event library and change jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setUpTests.js'],
   testEnvironment: 'jsdom',
+  /*
+   * https://stackoverflow.com/questions/54627028/jest-unexpected-token-when-importing-css
+   * https://www.npmjs.com/package/jest-transform-stub
+   */
+  moduleNameMapper: {
+    '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+  },
 };
