6e57b568ea19e66330ad2fc484e6939ee467729b
Vsevolod <sevkaplay@gmail.com>
2021-07-14 21:40:29 +0300

    Add setBooks api call in App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,15 @@
 import 'App/styles.scss';
 import { BooksPage } from 'pages/BooksPage/BooksPage';
+import { observer } from 'mobx-react';
+import { useStores } from 'hooks/useStores';
+import { useEffect } from 'react';
 
-const App = () => {
+const App = observer(() => {
+  const { bookStore } = useStores();
+  useEffect(() => {
+    bookStore.setBooks();
+  }, [bookStore]);
   return <BooksPage />;
-};
+});
 
 export { App };
