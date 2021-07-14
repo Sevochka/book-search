072c743263c2fef28a2ce1c48a93fab0231f3baa
Vsevolod <sevkaplay@gmail.com>
2021-07-14 21:32:11 +0300

    Add params to store and api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import { BooksData } from 'types';
 
 axios.defaults.baseURL = `https://www.googleapis.com/books/v1/volumes`;
 
 const loadBooksData = async (q: string) => {
-  const response = await axios.get('', {
-    params: { q, key: 'AIzaSyAD4odPQAWao6Hmf4Kdq7cnSoAFN3YL4jE' },
+  const response: AxiosResponse<BooksData> = await axios.get('', {
+    params: {
+      q,
+      key: 'AIzaSyAD4odPQAWao6Hmf4Kdq7cnSoAFN3YL4jE',
+      maxResults: 30,
+    },
   });
   return response.data;
 };
--- src/stores/BookStore.ts
+++ src/stores/BookStore.ts
@@ -1,13 +1,19 @@
-import { action, observable } from 'mobx';
+import { action, makeAutoObservable, observable } from 'mobx';
 import { loadBooksData } from 'api/api';
-import { BooksData } from 'types';
+import { BookItem } from 'types';
 
 class BookStore {
-  @observable books: BooksData[] | null = null;
+  @observable books: BookItem[] | null = null;
+  @observable totalItems: number | null = null;
+  @observable searchText = 'js';
+  constructor() {
+    makeAutoObservable(this);
+  }
 
   @action setBooks = (): void => {
-    loadBooksData('js').then((data) => {
-      this.books = data;
+    loadBooksData(this.searchText).then((data) => {
+      this.books = data.items;
+      this.totalItems = data.totalItems;
     });
   };
 }
