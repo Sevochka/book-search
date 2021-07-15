4d11b1a65e58e3b19001f204d58680667164cb0e
Vsevolod <sevkaplay@gmail.com>
2021-07-15 18:49:57 +0300

    Add withLoading to content part

--- src/pages/BooksPage/BooksPage.tsx
+++ src/pages/BooksPage/BooksPage.tsx
@@ -1,16 +1,22 @@
 import { ConfigurationPart } from 'components/ConfigarationPart/ConfigurationPart';
 import { ContentPart } from 'components/ContentPart/ContentPart';
 import './BooksPage.scss';
+import WithLoading from '../../hocs/withLoading';
+import { observer } from 'mobx-react';
+import { useStores } from 'hooks/useStores';
 
-const BooksPage = () => {
+const WithLoadingCardMainStat = WithLoading(ContentPart);
+
+const BooksPage = observer(() => {
+  const { bookStore } = useStores();
   return (
     <article className="books">
       <ConfigurationPart className="books__configuration" />
       <section className="books__content">
-        <ContentPart />
+        <WithLoadingCardMainStat isLoading={bookStore.isLoading} />
       </section>
     </article>
   );
-};
+});
 
 export { BooksPage };
