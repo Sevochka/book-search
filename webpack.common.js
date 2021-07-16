61ced9f0b32103a2a70bea93dab7b322188876c8
Vsevolod <sevkaplay@gmail.com>
2021-07-16 10:06:40 +0300

    Fix routing issue

--- webpack.common.js
+++ webpack.common.js
@@ -2,19 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, '..', './src/index.tsx'),
+  entry: path.resolve(__dirname, './src/index.tsx'),
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
     alias: {
-      components: path.resolve(__dirname, '..', './src/components'),
-      assets: path.resolve(__dirname, '..', './src/assets'),
-      hooks: path.resolve(__dirname, '..', './src/hooks'),
-      api: path.resolve(__dirname, '..', './src/api'),
-      data: path.resolve(__dirname, '..', './src/data'),
-      stores: path.resolve(__dirname, '..', './src/stores'),
-      App: path.resolve(__dirname, '..', './src/App'),
-      contexts: path.resolve(__dirname, '..', './src/contexts'),
-      pages: path.resolve(__dirname, '..', './src/pages'),
+      components: path.resolve(__dirname, './src/components'),
+      assets: path.resolve(__dirname, './src/assets'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+      api: path.resolve(__dirname, './src/api'),
+      data: path.resolve(__dirname, './src/data'),
+      stores: path.resolve(__dirname, './src/stores'),
+      App: path.resolve(__dirname, './src/App'),
+      contexts: path.resolve(__dirname, './src/contexts'),
+      pages: path.resolve(__dirname, './src/pages'),
+      routes: path.resolve(__dirname, './src/routes'),
     },
   },
   module: {
@@ -51,14 +52,15 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, '..', './build'),
+    path: path.resolve(__dirname, './build'),
     filename: 'bundle.js',
+    publicPath: '/',
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Books Browser',
       favicon: './src/assets/favicon.svg',
-      template: path.resolve(__dirname, '..', './src/index.html'),
+      template: path.resolve(__dirname, './src/index.html'),
     }),
   ],
 };
