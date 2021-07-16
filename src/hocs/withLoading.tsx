2eb4fd47813d0c06410789798e75b7d3b7808ea6
Vsevolod <sevkaplay@gmail.com>
2021-07-16 20:21:44 +0300

    Increase size of loading element

--- src/hocs/withLoading.tsx
+++ src/hocs/withLoading.tsx
@@ -11,7 +11,11 @@ const WithLoading = (Component: React.FC<any>) =>
       <Component {...props} />
     ) : (
       <div className="d-flex justify-content-center">
-        <div className="spinner-border text-primary" role="status">
+        <div
+          className="spinner-border text-primary mt-2"
+          style={{ width: '8rem', height: '8rem' }}
+          role="status"
+        >
           <span className="sr-only" />
         </div>
       </div>
