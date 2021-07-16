d08ed01c9f07db917a445deb5f6afa38ccb94d35
Vsevolod <sevkaplay@gmail.com>
2021-07-16 12:34:01 +0300

    Update BookPage with BookInfo component

--- src/pages/BookPage/BookPage.tsx
+++ src/pages/BookPage/BookPage.tsx
@@ -1,9 +1,48 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { observer } from 'mobx-react';
+import { useStores } from 'hooks/useStores';
+import withLoading from 'hocs/withLoading';
+import './BookPage.scss';
+import { useHistory } from 'react-router';
+import { BookInfo } from 'components/BookInfo/BookInfo';
 
-const BookPage: FC = () => {
-  const params = useParams();
-  return <h5>{JSON.stringify(params)}</h5>;
-};
+const WithLoadingBookInfo = withLoading(BookInfo);
+
+const BookPage: FC = observer(() => {
+  const { bookStore } = useStores();
+  const { id } = useParams<{ id: string }>();
+  const history = useHistory();
+  useEffect(() => {
+    bookStore.setCurrentBook(id);
+  }, [bookStore, id]);
+  const handleGoBack = () => {
+    bookStore.clearCurrentBook();
+    history.push('/');
+  };
+
+  const { currentBook } = bookStore;
+
+  return (
+    <>
+      <button
+        className="btn btn-outline-primary position-absolute m-1"
+        onClick={handleGoBack}
+      >
+        Return
+      </button>
+
+      <WithLoadingBookInfo
+        isLoading={!currentBook}
+        title={currentBook?.volumeInfo.title}
+        description={currentBook?.volumeInfo.description}
+        buyLink={currentBook?.saleInfo.buyLink}
+        thumbnail={currentBook?.volumeInfo.imageLinks?.thumbnail}
+        authors={currentBook?.volumeInfo.authors}
+        categories={currentBook?.volumeInfo.categories}
+      />
+    </>
+  );
+});
 
 export { BookPage };
